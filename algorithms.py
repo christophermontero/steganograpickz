8f2bc37bc939663b25d7c26fe035e9819535e28a
christophermontero <chris.to20@hotmail.com>
2020-11-08 17:47:42 -0500

    Defined method to expand session Key

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 from Crypto.PublicKey import RSA
+from Crypto.Hash import MD5
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
@@ -39,18 +40,32 @@ class CipherRSA():
 
 
 class CipherAES():
-    def __init__(self, text):
-        self.sessionKey = get_random_bytes(16)
+    def __init__(self, text, password):
+        self.password = password
         self.plainText = text
         self.plainText2Bytes = str.encode(self.plainText)
+        self.salt = None
+        self.sessionKey = None
         self.cipherText = None
         self.iv = None
         self.plainTextDecrypt = None
 
+    def expandSessionKey(self):
+        self.salt = get_random_bytes(16)
+        expandKey = str.encode(self.password) + self.salt
+
+        # Password is hashed
+        h = MD5.new()
+        h.update(expandKey)
+        self.sessionKey = h.digest()
+
+        return self.sessionKey
+
     def encrypted(self):
+        # Cipher AES is called
         cipher_aes = AES.new(self.sessionKey, AES.MODE_CBC)
         self.cipherText = cipher_aes.encrypt(pad(self.plainText2Bytes, AES.block_size))
-        self.iv = cipher_aes.iv
+        self.iv = cipher_aes.iv        
 
         return self.cipherText, self.iv
 
@@ -59,3 +74,4 @@ class CipherAES():
         self.plainTextDecrypt = unpad(cipherAES.decrypt(self.cipherText), AES.block_size).decode('utf-8')
 
         return self.plainTextDecrypt
+        
