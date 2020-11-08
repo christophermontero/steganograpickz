21b1a2e11880bfd7f916524e1378c70f63d46d60
christophermontero <chris.to20@hotmail.com>
2020-11-08 15:16:23 -0500

    Defined method to decrypt with RSA

--- algorithms.py
+++ algorithms.py
@@ -1,21 +1,36 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.Util.Padding import pad, unpad
 
-class algRSA():
-	self.publicKey = None
-	self.privateKey = None
 
-	def keyPair(self):
-		key RSA.generate(2048)
-		self.privateKey = key.export_key()
-		self.publicKey = key.publicKey().export_key()
+class AlgRSA():
+    def __init__(self):
+        self.publicKeyRSA = None
+        self.privateKeyRSA = None
+        self.encryptContent = None
+        self.decryptContent = None
 
-		return self.privateKey, self.publicKey
+    def keyPair(self):
+        key = RSA.generate(2048)
+        # Private key is being generated
+        self.privateKeyRSA = open("private.pem", "wb")
+        self.privateKeyRSA.write(key.export_key())
+        self.privateKeyRSA.close()
 
-	def encryptKey(self, keySession):
-		recipientKey = RSA.import_key(self.publicKey)
-		cipherRSA = PKCS1_OAEP.new(recipientKey)
-		encryptKey = cipherRSA.encrypt(keySession)
+        # Public key is being generated
+        self.publicKeyRSA = open("public.pem", "wb")
+        self.publicKeyRSA.write(key.publickey().export_key())
+        self.publicKeyRSA.close()
 
-		return encryptKey
+    def encrypt(self, content):
+        recipientKey = RSA.import_key(open("public.pem").read())
+        cipherRSA = PKCS1_OAEP.new(recipientKey)
+        self.encryptContent = cipherRSA.encrypt(content)
+
+        return self.encryptContent
+
+    def decrypt(self):
+        recipientKey = RSA.import_key(open("private.pem").read())
+        cipherRSA = PKCS1_OAEP.new(recipientKey)
+        self.decryptContent = cipherRSA.decrypt(self.encryptContent)
+
+        return self.decryptContent
