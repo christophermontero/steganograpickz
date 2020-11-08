24df0c01a97937ad23ddf7be6be426b70ce2b83b
christophermontero <chris.to20@hotmail.com>
2020-11-08 12:58:31 -0500

    Defined method to read the image

--- steganography.py
+++ steganography.py
@@ -1,4 +1,4 @@
-
+import cv2
 
 class Steganography:
     def __init__(self):
@@ -7,4 +7,12 @@ class Steganography:
         self.secretData = None
         
     def readImg(self, path):
+        self.image = cv2.imread(path)
+        self.noBytes = self.image.shape[0] * self.image.shape[1] * 3 // 8
         
+        
+stegano = Steganography()
+
+stegano.readImg("plain-text-password.jpg")
+
+print(stegano.noBytes)
