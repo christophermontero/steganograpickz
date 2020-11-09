ff20f1e1d4483f5f47288c842f131c208d28fbfc
christophermontero <chris.to20@hotmail.com>
2020-11-08 23:17:36 -0500

    Prepare the result to print the content hidden in the image

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import steganography as stegano
 
 
 # The message is readed
-fileUTF8 = open("message.txt","r")
+fileUTF8 = open("message.txt", "r")
 message = fileUTF8.read()
 fileUTF8.close()
 
@@ -15,7 +15,9 @@ aesCBC = alg.CipherAES(message, "password")
 aesCBC.expandSessionKey()
 messageEncryptAES = aesCBC.encrypted()
 
-dataAES = messageEncryptAES[0] + "." + messageEncryptAES[1] + "." + messageEncryptAES[2] # initial vector plus cipherText plus session Key
+# initial vector plus cipherText plus session Key
+dataAES = messageEncryptAES[0] + "." + \
+    messageEncryptAES[1] + "." + messageEncryptAES[2]
 
 # Encrypt AES with RSA
 rsa = alg.CipherRSA()
@@ -23,10 +25,10 @@ rsa.keyPair()
 messageEncryptRSA = rsa.encrypted(dataAES)
 
 # Key pairs are readed
-private = open("private.pem","r")
+private = open("private.pem", "r")
 priv = private.read()
 private.close()
-public = open("public.pem","r")
+public = open("public.pem", "r")
 pub = public.read()
 public.close()
 
@@ -35,23 +37,23 @@ image = stegano.Steganography()
 image.readImg("plain-text-password.jpg")
 secretContent = messageEncryptRSA + pub + image.filled(pub)
 imageEncrypted = image.encrypted(secretContent)
+
+# The image is been decrypted
 imageDecrypted = image.decrypted()
 dataAESEncrypt = secretContent[:256]
-
 cv2.imwrite("image-tampered.PNG", imageEncrypted)
 
 # Decrypt with RSA
 messageDecryptRSA = rsa.decrypted(dataAESEncrypt)
 splitMessage = messageDecryptRSA.split('.')
-cipherText = splitMessage[0]
-iv = splitMessage[1]
-sessionKey = splitMessage[2]
 
 # Decrypt RSA with AES
-messageDecryptAES = aesCBC.decrypted(splitMessage[0],splitMessage[1],splitMessage[2])
-print(messageDecryptAES)
-'''
-cv2.imshow('Image tampered', imageEncrypted)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
+messageDecryptAES = aesCBC.decrypted(splitMessage[0], splitMessage[1], splitMessage[2])
+
+output = ("Cipher text: " + b64encode(splitMessage[0]).decode('utf-8') + "\n" +
+          "Initial vector: " + b64encode(splitMessage[1]).decode('utf-8') + "\n" +
+          "Session key: " + b64encode(splitMessage[2]).decode('utf-8') + "\n" +
+          "Message: " + messageDecryptAES + "\n"
+          )
+
+print(output)
